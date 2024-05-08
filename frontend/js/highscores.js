08ac3933f55e456491e9b873486756829c5a56f5
Aaron <a.genevision14@gmail.com>
2024-05-07 20:59:18 -0500

    Developed highscores.html elements, styling, and functionality

--- frontend/js/highscores.js
+++ frontend/js/highscores.js
@@ -2,5 +2,5 @@ const highScoresList = document.getElementById('highScoresList');
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
 highScoresList.innerHTML = highScores.map( score => {
-    return `<li class="list-group-item">${score.user}: ${score.score}</li>`;
+    return `<li class="high-score">${score.name}: ${score.score}</li>`;
 }).join('');
